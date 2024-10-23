5199aed985398ef9deed20eaac11973f4c794a60
Kant Chen <kant.chen.yan.cheng@gmail.com>
2024-10-23 10:24:53 +0800

    Chore: code formatting

--- tasks/test_enqueue.py
+++ tasks/test_enqueue.py
@@ -11,7 +11,7 @@ async def enqueue_letters_a_to_z():
         print("enqueued", i)
         task = asyncio.create_task(push_message(settings.REDIS_QUEUE_NAME, i))
         tasks.append(task)
-        
+
     results = await asyncio.gather(*tasks)
     await close_redis_connection()
     return results
@@ -20,6 +20,7 @@ async def enqueue_letters_a_to_z():
 def test_enqueue():
     result = asyncio.run(enqueue_letters_a_to_z())
     print(result)
+
 
 if __name__ == "__main__":
     test_enqueue()
